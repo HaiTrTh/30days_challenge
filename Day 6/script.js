a26acd0c2cd7517702c9d4514004d3e93c09a48d
haitrth <haithanhtran00@gmail.com>
2024-08-12 21:58:57 +0700

    day6

---
+++ Day 6/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown',function(event){
+   document.querySelector('.btn-key').classList.add('hide')
+   document.querySelector('.key').classList.remove('hide')
+   document.querySelector('.key__info p:nth-child(2)').innerHTML = event.key
+   document.querySelector('.key__locations p:nth-child(2)').innerHTML = event.location
+   document.querySelector('.key__which p:nth-child(2)').innerHTML = event.keyCode
+   document.querySelector('.key__code p:nth-child(2)').innerHTML = event.code
+})
