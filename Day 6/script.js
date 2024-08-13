08097ab18cf847f91dc1d47a62d9f012757a93d8
haitrth <haithanhtran00@gmail.com>
2024-08-13 11:21:52 +0700

    edit

--- Day 6/script.js
+++ Day 6/script.js
@@ -2,6 +2,7 @@ document.addEventListener('keydown',function(event){
   
    document.querySelector('.btn-key').classList.add('hide')
    document.querySelector('.key').classList.remove('hide')
+   document.querySelector('.key h3').innerHTML = event.keyCode
    document.querySelector('.key__info p:nth-child(2)').innerHTML = event.key
    document.querySelector('.key__locations p:nth-child(2)').innerHTML = event.location
    document.querySelector('.key__which p:nth-child(2)').innerHTML = event.keyCode
