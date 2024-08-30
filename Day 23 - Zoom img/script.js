b7930f816b47935dc4d055e3ea68cf4a762de0e2
haitrth <haithanhtran00@gmail.com>
2024-08-30 13:22:05 +0700

    day 23 Zoom img

--- Day 23 - Zoom img/script.js
+++ Day 23 - Zoom img/script.js
@@ -1,6 +1,28 @@
-var result = document.querySelector('#result') 
 
-document.addEventListener('mousemove', function(e){
-    result.style.top = `${e.clientY}px`
-    result.style.left = `${e.clientX}px`
+var mirror = document.getElementById("mirror")
+var imgs = document.querySelectorAll('.img-wraper .img')
+imgs.forEach((img) => {
+    img.addEventListener('mousemove', function(e){
+        
+        var imgSrc = this.getAttribute('src');
+        console.log(imgSrc);
+        // console.log()
+        let w = this.offsetWidth
+        let h = this.offsetHeight
+    
+        let mouseMoveOffsetImgWidth = e.pageX - this.offsetLeft;
+        let mouseMoveOffsetImgHeight = e.pageY - this.offsetTop;
+        // console.log(mouseMoveOffsetImgWidth)
+        // console.log(mouseMoveOffsetImgHeight)
+        let left = [(mouseMoveOffsetImgWidth / w) * 100]  
+        let top = [(mouseMoveOffsetImgHeight / h) * 100]  
+        
+         mirror.style.backgroundImage = `url(${imgSrc})`
+         mirror.style.backgroundPositionX = `${left}% `
+         mirror.style.backgroundPositionY = `${top}% `
+
+         mirror.style.top = `${e.pageY}px`;
+         mirror.style.left = `${e.pageX}px`;
+
+    })
 })
